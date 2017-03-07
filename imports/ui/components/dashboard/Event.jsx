d746af40d1296254fae33ccfea3feccbdc39f981
Unknown <subhan70m@gmail.com>
2017-03-07 22:51:48 +0000

    c

--- imports/ui/components/dashboard/Event.jsx
+++ imports/ui/components/dashboard/Event.jsx
@@ -1,50 +1,42 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
-import {createContainer} from 'meteor/react-meteor-data';
+import { createContainer } from 'meteor/react-meteor-data';
 import $ from 'jquery';
 import ReactDOM from 'react-dom';
 
-import {Events} from '../../../api/events';
+import { Events } from '../../../api/events';
 
 class Event extends Component {
-    constructor() {
-        super();
-        this.deleteEvent = this
-            .deleteEvent
-            .bind(this);
-    }
-    deleteEvent(){
-            const eventId = this.props.event._id;
-            const card = ReactDOM.findDOMNode(this.refs.eventId);
-            console.log(eventId);
-            Events.remove(this.props.event._id);
-    }
-    render( ) {
-        const currentUser = this.props.currentUser || ''
-        console.log(currentUser);
-        let isAdmin = currentUser && Roles.userIsInRole( currentUser, 'admin' );
-        console.log( isAdmin );
-
-        const _id = this.props.event._id;
-
-        return (    
-            <div className="card" ref="dvsv">
-                <div className="card-content">
-                    <span className="card-title">{this.props.event.title} {isAdmin
-                            ? <span className="right" ref="dbdfb">
-                                    <a href="" onClick={this.deleteEvent}><i className="material-icons red-text">clear</i></a>
-                                </span>
-                            : ''}
-                    </span>
-                    <p>{this.props.event.text}</p>
-                </div>
-                <div className="card-action">
-                    <a href="#" className="green-text">Confirm</a>
-                    <a href="#" className="red-text">Give apology</a>
-                </div>
+   constructor( ) {
+      super( );
+      this.deleteEvent = this.deleteEvent.bind( this );
+   }
+   deleteEvent( ) {
+      Events.remove( this.props.event._id );
+   }
+   render( ) {
+      const currentUser = this.props.currentUser || ''
+      let isAdmin = currentUser && Roles.userIsInRole( currentUser, 'admin' );
+      return (
+         <div className="card" ref="dvsv">
+            <div className="card-content">
+               <span className="card-title">{this.props.event.title} {isAdmin
+                     ? <span className="right" ref="dbdfb">
+                           <a href="" onClick={this.deleteEvent}>
+                              <i className="material-icons red-text">clear</i>
+                           </a>
+                        </span>
+                     : ''}
+               </span>
+               <p>{this.props.event.text}</p>
             </div>
-        )
-    }
+            <div className="card-action">
+               <a href="#" className="green-text">Confirm</a>
+               <a href="#" className="red-text">Give apology</a>
+            </div>
+         </div>
+      )
+   }
 }
 
 export default createContainer( ( ) => {
